1b4f29977e79705f7b05072c637033864d9cf77b
Sharon Chiang <sharon@Sharons-MacBook-Pro-404.local>
2024-02-18 22:40:11 -0800

    Reformatted Ch6 code

--- Chapter 06/Ch6_code.R
+++ Chapter 06/Ch6_code.R
@@ -1,4 +1,4 @@
-# Import libraries (install if not already installed)
+# Import libraries 
 library(survival)
 library(icenReg)
 library(ggplot2)
@@ -25,7 +25,7 @@ censor = as.numeric(!is.na(survtime))
 
 # Kaplan-Meier estimator
 fits =survfit (Surv(survtime, censor) ~  Group,  data=data.frame(Dsurv))
-ggsurv <- ggsurvplot(fits , data =Dsurv,conf.int = TRUE,  risk.table =T,  
+ggsurv <- ggsurvplot(fits , data = Dsurv,conf.int = TRUE,  risk.table =T,  
                      ggtheme = theme_light(), pval =F, conf.int.style = "step", 
                      legend.lab =  c( 'Medical-therapy group', 'Surgery group'), 
                      palette = gray(seq(0.5,0, len=2)), xlab = 'Time (days)')
@@ -36,12 +36,12 @@ surv_diff <- survdiff(Surv(survtime, censor) ~ Group,  data=data.frame(Dsurv))
 
 # Cox proportional hazards model
 newdata = Dsurv[c(1,4),  ]
-coxfits =coxph(Surv(survtime, censor) ~ Group,  data=data.frame(Dsurv))
+coxfits = coxph(Surv(survtime, censor) ~ Group,  data=data.frame(Dsurv))
 summary(coxfits)
 
 # Cox proportional hazards model with additional covariates
 coxcovfits = coxph(Surv(survtime, censor) ~  Group + Age + Gender + Diag + 
-                    Family_hist,data = data.frame(Dsurv))
+                    Family_hist, data = data.frame(Dsurv))
 plot(residuals(coxcovfits, method = 'deviance'), ylab = 'Martingale residual')
 
 ggsurv <- ggsurvplot(survfit(coxfits, newdata = newdata), data = Dsurv,
@@ -57,11 +57,19 @@ test.ph <- cox.zph(coxfits)
 testcov.ph <- cox.zph(coxcovfits)
 
 # Parametric model
-regfits =survreg(Surv(survtime, censor) ~  Group,  data=data.frame(Dsurv), dist = 'lognormal')
+regfits =survreg(Surv(survtime, censor) ~  Group,  data=data.frame(Dsurv), 
+                 dist = 'lognormal')
 
 # Parametric model with additional covariates
-regcovfits =survreg(Surv(survtime, censor) ~  Group +Age + Gender +Diag + Family_hist ,  data=data.frame(Dsurv), dist = 'lognormal')
-ggsurvreg <- ggsurvplot(predict(regfits), data =Dsurv,conf.int = TRUE,  ggtheme = theme_light(), pval =F, conf.int.style = "step", legend.lab = c( 'Medical-therapy group', 'Surgery group'), palette = gray(seq(0.5,0, len=2)), xlab = 'Time (days)')
+regcovfits = survreg(Surv(survtime, censor) ~  Group + Age + Gender + Diag + 
+                      Family_hist,  data = data.frame(Dsurv), 
+                     dist = 'lognormal')
+ggsurvreg <- ggsurvplot(predict(regfits), data = Dsurv, conf.int = TRUE,  
+                        ggtheme = theme_light(), pval = F, 
+                        conf.int.style = "step", 
+                        legend.lab = c('Medical-therapy group', 'Surgery group'), 
+                        palette = gray(seq(0.5,0, len = 2)), 
+                        xlab = 'Time (days)')
 print(ggsurvreg)
 
 summary(regcovfits)
@@ -110,17 +118,21 @@ wt2 = weib(t, 1.5, 0.8)
 expn = expon(t, 1)
 logres = lognorm(t, 0,5)
 
-plot(wt1[1, ]~t, type = 'l', lwd = 3, lty = 1, ylim = c(0, 4), xlab = 'time', ylab = 'Hazard function')
+plot(wt1[1, ] ~ t, type = 'l', lwd = 3, lty = 1, ylim = c(0, 4), xlab = 'time', 
+     ylab = 'Hazard function')
 
 lines(wt2[1, ]~t, type = 'l', lwd = 3, lty = 2)
 lines(expn[1, ]~t, type = 'l', lwd = 3, lty = 3)
 lines(logres[1, ]~t, type = 'l', lwd = 3, lty = 4)
-legend('topleft',c('Increasing Weibull', 'Decreasing Weibull', 'Exponential', 'Log-normal'), lty = 1:4, )
+legend('topleft',c('Increasing Weibull', 'Decreasing Weibull', 'Exponential', 
+                   'Log-normal'), lty = 1:4, )
 
-plot(wt1[2, ]~t, type = 'l', lwd = 3, lty = 1, ylim = c(0, 1.2), xlab = 'time', ylab = 'Survival function')
+plot(wt1[2, ] ~ t, type = 'l', lwd = 3, lty = 1, ylim = c(0, 1.2), xlab = 'time', 
+     ylab = 'Survival function')
 
-lines(wt2[2, ]~t, type = 'l', lwd = 3, lty = 2)
-lines(expn[2, ]~t, type = 'l', lwd = 3, lty = 3)
-lines(logres[2, ]~t, type = 'l', lwd = 3, lty = 4)
-legend('topleft',c('Increasing Weibull', 'Decreasing Weibull', 'Exponential', 'Log-normal'), lty = 1:4, )
+lines(wt2[2, ] ~ t, type = 'l', lwd = 3, lty = 2)
+lines(expn[2, ] ~ t, type = 'l', lwd = 3, lty = 3)
+lines(logres[2, ] ~ t, type = 'l', lwd = 3, lty = 4)
+legend('topleft', c('Increasing Weibull', 'Decreasing Weibull', 'Exponential', 
+                   'Log-normal'), lty = 1:4, )
 
